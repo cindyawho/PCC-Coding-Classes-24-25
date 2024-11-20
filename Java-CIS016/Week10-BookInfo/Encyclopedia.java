d973f110bc3f113fd8becdf1c9dcdf5c142f2895
Cindy Andrade <cindya3939@gmail.com>
2024-11-20 09:55:17 -0800

    Add lab about overriding member methods

---
+++ Java-CIS016/Week10-BookInfo/Encyclopedia.java
@@ -0,0 +1,37 @@
+public class Encyclopedia extends Book {
+    // TODO: Declare private fields
+    private String edition;
+    private int numPages;
+    
+    // TODO: Define mutator methods - 
+    //       setEdition(), setNumPages()
+    public void setEdition(String userIn){
+        edition = userIn;
+    }
+    public void setNumPages(int userIn){
+        numPages = userIn;
+    }
+    
+    // TODO: Define accessor methods -
+    //       getEdition(), getNumPages()
+    public String getEdition(){
+        return edition;
+    }
+    public int getNumPages(){
+        return numPages;
+    }
+    
+    // TODO: Define a printInfo() method that overrides 
+    //       the printInfo in Book class 
+    @Override
+    public void printInfo() {
+        System.out.println("Book Information: ");
+        System.out.println("   Book Title: " + title);
+        System.out.println("   Author: " + author);
+        System.out.println("   Publisher: " + publisher);
+        System.out.println("   Publication Date: " + publicationDate);
+        System.out.println("   Edition: " + edition);
+        System.out.println("   Number of Pages: " + numPages);
+    }
+   
+}
